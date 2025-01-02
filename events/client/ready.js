b4c2f536d14286b7183b5af63a57fdc588f55e68
anusara-bemal <146241166+anusara-bemal@users.noreply.github.com>
2025-01-02 22:32:23 +0530

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,7 +2,7 @@ module.exports = async (client) => {
 	console.log(`Logged in as ${client.user.tag}!`);
 
 	await client.user.setPresence({ 
-		activities: [{ name: `Made by: stylish.`, type: 2 }], 
+		activities: [{ name: `Made by: Anusara Bemal`, type: 2 }], 
 		status: 'online', 
 	});
 }
